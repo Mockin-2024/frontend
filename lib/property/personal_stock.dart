1e017ce82b260064167e7dffe1c8970f011f0f18
KimHyeokMin0 <soulet76@naver.com>
2024-10-30 16:58:04 +0900

    주문내역 수정_2

--- lib/property/personal_stock.dart
+++ lib/property/personal_stock.dart
@@ -119,7 +119,7 @@ class PersonalStock extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const OrderList(),
+                    builder: (context) => OrderList(),
                   ),
                 );
               },
