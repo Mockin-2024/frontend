f1edd39c75171e2696450cc382d22e97c6db949c
KimHyeokMin0 <soulet76@naver.com>
2024-11-06 15:40:14 +0900

    앞으로 추가할 것들

---
+++ lib/stocks/detailed/more_info_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoreInfoTab extends StatelessWidget {
+  const MoreInfoTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/stocks/detailed/my_stock_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyStockTab extends StatelessWidget {
+  const MyStockTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
