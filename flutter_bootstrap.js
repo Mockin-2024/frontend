3ef919de1ab257a4a823fddde24758a26260d48a
KimHyeokMin0 <KimHyeokMin0@users.noreply.github.com>
2024-11-14 07:20:18 +0000

    deploy: 9280ef8ef274b8f7bfbdee0e1ccf7f28907f4164

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "11384203"
+    serviceWorkerVersion: "945354765"
   }
 });
